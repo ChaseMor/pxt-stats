6152eabea5929c61d7ab7c966e914abcc537b926
ChaseMor <C_Mortensen@live.cm>
2018-12-06 15:50:04 -0800

    Added common probability functions

--- stats.ts
+++ stats.ts
@@ -1,20 +1,54 @@
 namespace stats {
+
     /**
-     * Gets a random number generated using a specified given normal (Gaussian) distribution
-     * @param mean the mean value of the distribution
-     * @param std the standard deviation of the distribution
-     * @returns a random number generated with the probability denisity of a normal distribution
+     * Gets the factorial of the number given
+     * @param n the number the operation is perfomed on
+     * @returns the factorial of the number given
      */
-    export function randomNorm(mean: number, std: number): number {
-        // Uses a Box-Muller Transform to generate a normally distributed sampling
-        let s = 0;
-        let v1: number = Math.random();
-        let v2: number = Math.random();
-        return std * (Math.sqrt(-2 * Math.log(v2)) * Math.cos(2 * Math.PI * v1)) + mean;
-        /*
-        let multiplier = Math.sqrt(-2 * Math.log(s) / s);
-        //console.logValue("v1", v1);
-        return Math.trunc(v1 * std * multiplier) + mean;
-        */
+    export function factorial(n: number): number {
+        if (n < 0) {
+            return -1;
+        }
+        let output: number = 1;
+        for (let i = 1; i <= n; i++) {
+            output *= i;
+        }
+        return output;
+    }
+
+    /**
+     * Gets the number of permutations of given the total number of 
+     * options and the how many are used/chosen.
+     * Will result in -1 if more is chosen then are used
+     *
+     * @param total the total number of possibilities
+     * @param chosen the number of possibilites used
+     * @returns the number of permutations possible
+     */
+    export function permutations(total: number, chosen: number): number {
+        if (chosen > total) {
+            return -1;
+        }
+        let output: number = 1;
+        for (let i = total; i > total - chosen; i--) {
+            output *= i;
+        }
+        return output;
+    }
+
+    /**
+     * Gets the number of combination of given the total number of 
+     * options and the how many are used/chosen.
+     * Will result in -1 if more is chosen then are used
+     *
+     * @param total the total number of possibilities
+     * @param chosen the number of possibilites chosen
+     * @returns the number of combinations possible
+     */
+    export function choose(total: number, chosen: number): number {
+        if (chosen > total) {
+            return -1;
+        }
+        return Math.idiv(permutations(total, chosen), factorial(chosen));
     }
 }
