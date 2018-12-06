d807a8f33e32ddd0fb930abd987b4c786a2080d6
ChaseMor <C_Mortensen@live.cm>
2018-12-05 22:58:25 -0800

    Added generation of normally distributed random numbers

---
+++ stats.ts
@@ -0,0 +1,20 @@
+namespace stats {
+    /**
+     * Gets a random number generated using a specified given normal (Gaussian) distribution
+     * @param mean the mean value of the distribution
+     * @param std the standard deviation of the distribution
+     * @returns a random number generated with the probability denisity of a normal distribution
+     */
+    export function randomNorm(mean: number, std: number): number {
+        // Uses a Box-Muller Transform to generate a normally distributed sampling
+        let s = 0;
+        let v1: number = Math.random();
+        let v2: number = Math.random();
+        return std * (Math.sqrt(-2 * Math.log(v2)) * Math.cos(2 * Math.PI * v1)) + mean;
+        /*
+        let multiplier = Math.sqrt(-2 * Math.log(s) / s);
+        //console.logValue("v1", v1);
+        return Math.trunc(v1 * std * multiplier) + mean;
+        */
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// tests go here; this will not be compiled when this package is used as a library
+let arr: number[] = [];
+
+for (let i = 0; i < 160; i++) {
+    arr.push(1);
+}
+
+for (let i = 0; i < 20000; i++) {
+    let num: number = Math.trunc(stats.randomNorm(80, 10));
+
+    if (num >= 0 && num < 160)
+        arr[num] += 0.1;
+}
+
+game.onPaint(function () {
+    for (let i = 0; i < arr.length; i++) {
+        screen.setPixel(i, 120 - arr[i], 1);
+    }
+})
