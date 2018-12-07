34ecdcaf818ccbcdbefcbd13aa584b2991ab75e0
ChaseMor <C_Mortensen@live.cm>
2018-12-06 16:37:39 -0800

    Created framework for Distribution objects

--- distributions.ts
+++ distributions.ts
@@ -1,15 +1,106 @@
 namespace stats {
+
     /**
-     * Gets a random number generated using a specified given normal (Gaussian) distribution
-     * @param mean the mean value of the distribution
-     * @param std the standard deviation of the distribution
-     * @returns a random number generated with the probability denisity of a normal distribution
+     * Defines the usage of a Distribution
      */
-    export function randomNorm(mean: number, std: number): number {
-        // Uses a Box-Muller Transform to generate a normally distributed sampling
-        let s = 0;
-        let v1: number = Math.random();
-        let v2: number = Math.random();
-        return std * (Math.sqrt(-2 * Math.log(v2)) * Math.cos(2 * Math.PI * v1)) + mean;
+    interface Distribution {
+
+        /**
+         * Generates a random number
+         */
+        generateRandom(): number;
+
+        /**
+         * Gets the mean value of the distribution
+         */
+        mean(): number;
+
+        /**
+         * Gets the standard deviation of the distribution
+         */
+        standardDeviation(): number;
+
+        /**
+         * Gets the variance of the distribution
+         */
+        variance(): number;
+    }
+
+    /**
+     * A Distribution that generates numbers based on a normal distribution
+     */
+    export class NormalDistribution implements Distribution {
+        
+        /**
+         * The mean (or average) value of the distribution
+         */
+        private avg: number;
+
+        /**
+         * The standard deviation of the distribution
+         */
+        private std: number;
+
+
+        /**
+         * The next value to be returned
+         */
+        private nextGauss: number;
+
+        /**
+         * Creates a new Normal Distribution number generator with the given properties
+         * 
+         * @param mean the mean value of the distribution
+         * @param std the standard deviation of the distribution
+        */
+        constructor(mean: number, std: number) {
+            this.avg = mean;
+            this.std = std;
+            this.nextGauss = undefined;
+        }
+        /**
+         * Gets a random number generated using a specified given normal (Gaussian) distribution
+         * 
+         * @returns a random number generated with the probability denisity of a normal distribution
+         */
+        generateRandom(): number {
+            if (this.nextGauss) {
+                const out: number = this.nextGauss;
+                this.nextGauss = undefined;
+                return out;
+            }
+            // Uses a Box-Muller Transform to generate a normally distributed sampling
+            let v1: number = Math.random();
+            let v2: number = Math.random();
+            this.nextGauss = this.std * (Math.sqrt(-2 * Math.log(v1)) * Math.cos(2 * Math.PI * v2)) + this.avg;
+            return this.std * (Math.sqrt(-2 * Math.log(v2)) * Math.cos(2 * Math.PI * v1)) + this.avg;
+        }
+
+        /**
+         * Gets the mean value of the distribution
+         * 
+         * @returns the mean value of the distribution
+         */
+        mean(): number {
+            return this.avg;
+        }
+
+        /**
+         * Gets the standard deviation of the distribution
+         * 
+         * @returns the standard deviation of the distribution
+         */
+        standardDeviation(): number {
+            return this.std;
+        }
+        
+        /**
+         * Gets the variance of the distribution
+         * 
+         * @returns the variance of the distribution
+         */
+        variance(): number {
+            return this.std * this.std;
+        }
     }
 }
--- test.ts
+++ test.ts
@@ -4,11 +4,14 @@ for (let i = 0; i < 160; i++) {
     arr.push(1);
 }
 
+let normGen: stats.NormalDistribution = new stats.NormalDistribution(80, 10);
 for (let i = 0; i < 20000; i++) {
-    let num: number = Math.trunc(stats.randomNorm(80, 10));
+    let num: number = Math.trunc(normGen.generateRandom());
 
-    if (num >= 0 && num < 160)
+    if (num >= 0 && num < 160) {
         arr[num] += 0.1;
+    }
+
 }
 
 game.onPaint(function () {
